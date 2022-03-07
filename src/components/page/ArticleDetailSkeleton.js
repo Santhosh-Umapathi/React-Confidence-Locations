16283ad90fb9218b2ebe148fe500340db27dd351
Santhosh Umapathi <Santhosh.Professional1@gmail.com>
2022-03-07 16:54:19 +0100

    FIX: Small fix to mobile skeleton

--- src/components/page/ArticleDetailSkeleton.js
+++ src/components/page/ArticleDetailSkeleton.js
@@ -13,46 +13,46 @@ const ArticleDetailSkeleton = () => (
 
     <div className="flex flex-col space-y-3 p-5 w-full">
       <Skeleton
-        css="flex w-2/3 h-12"
+        css="flex w-2/3 h-6 sm:h-12"
         baseColor="#24292F"
         highlightColor="#0D1116"
       />
 
       <div className="flex flex-col space-y-3">
         <Skeleton
-          css="flex w-60 h-6"
+          css="flex w-60 h-3 sm:h-6"
           baseColor="#24292F"
           highlightColor="#0D1116"
         />
         <Skeleton
-          css="flex w-60 h-6"
+          css="flex w-60 h-3 sm:h-6"
           baseColor="#24292F"
           highlightColor="#0D1116"
         />
         <Skeleton
-          css="flex w-60 h-6"
+          css="flex w-60 h-3 sm:h-6"
           baseColor="#24292F"
           highlightColor="#0D1116"
         />
         <Skeleton
-          css="flex w-60 h-6"
+          css="flex w-60 h-3 sm:h-6"
           baseColor="#24292F"
           highlightColor="#0D1116"
         />
         <Skeleton
-          css="flex w-60 h-6"
+          css="flex w-60 h-3 sm:h-6"
           baseColor="#24292F"
           highlightColor="#0D1116"
         />
 
         <Skeleton
-          css="flex w-60 h-6"
+          css="flex w-60 h-3 sm:h-6"
           baseColor="#24292F"
           highlightColor="#0D1116"
         />
       </div>
       <Skeleton
-        css="flex w-72 h-36"
+        css="flex w-72 h-20 sm:h-36"
         baseColor="#24292F"
         highlightColor="#0D1116"
       />
